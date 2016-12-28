902f1ce8ce6d44bbaf8e15852bccd5af17ed23b1
yxzhou <sialvsic@outlook.com>
2016-12-28 10:08:21 +0800

    [YX] add syncHistoryWithStore and react-route

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './containers/App';
+import getStoreInstance from './store';
 import reducer from './reducers';
 import './Stylesheet/index.scss';
 
-const store = createStore(reducer);
+const store = getStoreInstance(reducer);
+let history = syncHistoryWithStore(browserHistory, store);
 
 class Root extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <App />
+        <Router history={history}>
+          <Route path="/" component={App}>
+          </Route>
+        </Router>
       </Provider>);
   }
 }
---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger'
+import rootReducer from '../reducers';
+
+let storeInstance = null;
+const logger = createLogger();
+
+const configureStore = function () {
+    storeInstance = createStore(
+        rootReducer,
+        compose(
+            applyMiddleware(thunk, logger),
+            window.devToolsExtension ? window.devToolsExtension() : f => f
+        )
+    );
+
+    return storeInstance;
+};
+
+const getStoreInstance = function () {
+    return storeInstance ? storeInstance : configureStore();
+};
+
+export default getStoreInstance;
+
